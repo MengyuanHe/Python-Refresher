a5d686a4aebf350ec375d0b685950ccd45f71813
MengyuanHe <72575835+MengyuanHe@users.noreply.github.com>
2020-12-05 20:55:06 -0500

    Add files via upload

--- plot_function.py
+++ plot_function.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0,2*np.pi,0.1)   # start,stop,step
-y = np.sin(x)
-z = np.cos(x)
+def plot():
+    x = np.arange(0,2*np.pi,0.1)   # start,stop,step
+    y = np.sin(x)
+    z = np.cos(x)
+    plt.plot(x,y,x,z)
+    plt.show()
 
-plt.plot(x,y,x,z)
-plt.show()
+if __name__ == "__main__":
+    plot()
